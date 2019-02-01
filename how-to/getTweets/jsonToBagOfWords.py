e4926ea3a6adda950d9dec4c09fa97a5f719762b
Cord Meados <meadosc@gmail.com>
2019-01-31 22:19:20 -0800

    creates a vectorized array. Needs to be verified for accuracy.

--- how-to/getTweets/jsonToBagOfWords.py
+++ how-to/getTweets/jsonToBagOfWords.py
@@ -7,8 +7,30 @@ import pandas as pd
 
 from sklearn.feature_extraction.text import CountVectorizer
 
-df = pd.read_json('data.json',orient='values')
-print(df.head())
+# Load the data to a pandas dataframe object
+df = pd.read_json('data.json', orient='values')
+
+# Get some basic info, like an example row and the column headers
+"""
+print("\nFirst Row:")
+print(df.head(1))
+print("\n Column Headers:")
+print(df.columns.values)
+"""
+
+# separate the text column for now.
+textData = df['text']
+
+# create a vectorizer and start preparing the data.
+vectorizer = CountVectorizer()
+vectorizer.fit(textData) # .fit() finds every unique word to create a word array
+# print(vectorizer.vocabulary_) # getting a look at the data. Debug.
+vector = vectorizer.transform(textData)
+print(vector.shape)
+print(type(vector))
+print(vector.toarray())
 
 
 # https://stackoverflow.com/questions/48806010/bag-of-words-with-json-array
+# https://scikit-learn.org/stable/modules/generated/sklearn.feature_extraction.text.CountVectorizer.html
+# https://machinelearningmastery.com/prepare-text-data-machine-learning-scikit-learn/
