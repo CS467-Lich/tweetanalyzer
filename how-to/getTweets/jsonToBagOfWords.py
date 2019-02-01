2a7d2540f371846ae5d6639b1313c36c79f1e07d
SandhyaSavitaRaman <SandhyaSavitaRaman@users.noreply.github.com>
2019-02-01 01:41:26 -0800

    Update jsonToBagOfWords.py
    minor adjustments so we can read the vocab, checking output of a new string based on our bag of words.

--- how-to/getTweets/jsonToBagOfWords.py
+++ how-to/getTweets/jsonToBagOfWords.py
@@ -22,14 +22,15 @@ print(df.columns.values)
 textData = df['text']
 
 # create a vectorizer and start preparing the data.
-vectorizer = CountVectorizer()
+vectorizer = CountVectorizer(strip_accents='ascii', ngram_range=(1,2)) #playing around with ngram - we can definitely keep it simple and get rid of this
 vectorizer.fit(textData) # .fit() finds every unique word to create a word array
-# print(vectorizer.vocabulary_) # getting a look at the data. Debug.
+print(vectorizer.vocabulary_) # getting a look at the data.
 vector = vectorizer.transform(textData)
 print(vector.shape)
 print(type(vector))
 print(vector.toarray())
 
+print(vectorizer.transform(['peta talks about 11 dogs weighing 10kg a lot.']).toarray()) #checking to see if the array for this text starts with [1, 1, ..] - Verified
 
 # https://stackoverflow.com/questions/48806010/bag-of-words-with-json-array
 # https://scikit-learn.org/stable/modules/generated/sklearn.feature_extraction.text.CountVectorizer.html
