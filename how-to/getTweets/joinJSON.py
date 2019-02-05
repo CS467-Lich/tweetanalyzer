687151ba9544166a1f3f01f45f696c25675dee84
Cord Meados <meadosc@gmail.com>
2019-02-04 19:21:44 -0800

    added joinJSON.py. This program combines multiple json files into a single json file. possible bug as my computer doesn't read the final file as JSON, but it looked good on visual review.

---
+++ how-to/getTweets/joinJSON.py
@@ -0,0 +1,46 @@
+'''
+Joining JSON files in folder.
+CS467 Lich
+Cord Meados 2019
+'''
+
+import json
+import os
+from helperFunctions import writeAsJSON
+from jsonmerge import Merger
+
+
+MY_DATA_FOLDER = "joinJSON/"
+MY_DATA_SUBFOLDER = "Political"
+MY_FINAL_DATA = 'Political_Final_Positive'
+
+# storing all the json data here
+finalJSON = {}
+
+# Prepare merger. https://pypi.org/project/jsonmerge/
+schema = {
+        "properties": {
+            "user": {"mergeStrategy": "append"},
+            "date": {"mergeStrategy": "append"},
+            "text": {"mergeStrategy": "append"},
+            "source": {"mergeStrategy": "append"},
+            "coordinates": {"mergeStrategy": "append"},
+            "language": {"mergeStrategy": "append"},
+            "hashtags": {"mergeStrategy": "append"},
+        }
+}
+merger = Merger(schema)
+
+# import json data files and join them into "finalJSON"
+for filename in os.listdir(MY_DATA_FOLDER + '/' + MY_DATA_SUBFOLDER):
+    # print(filename)
+    with open(MY_DATA_FOLDER + '/' + MY_DATA_SUBFOLDER + '/' + filename) as f:
+        tempJSONData = json.load(f)
+    finalJSON = merger.merge(finalJSON, tempJSONData)
+
+# store final json data
+writeAsJSON(finalJSON, MY_DATA_FOLDER + '/' + MY_DATA_SUBFOLDER + '/' + MY_FINAL_DATA)
+
+
+# References
+# https://pypi.org/project/jsonmerge/
